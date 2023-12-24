2507b8cd12b5bec24cd249d38d21a26dadd60215
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-12-24 20:54:04 +0800

    Able to start the mariadb container

---
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sqlcommands="
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+"
+
+mysql -u root -p $MYSQL_ROOT_PASSWORD -e "$sqlcommands"
+
+exec "$@"
