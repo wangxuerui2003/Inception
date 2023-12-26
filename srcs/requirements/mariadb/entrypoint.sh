ee5661f570148323f95f7aff714b1b45d4a690c7
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-12-26 11:46:32 +0800

    Done mariadb

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
 	chown -R mysql:mysql /var/lib/mysql 
 
 	# init database
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql > /dev/null
 
-	mysql_secure_installtion_script="
+	mysql_secure_installation_script="
+	FLUSH PRIVILEGES;
 	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 	DROP USER ''@'localhost';
-	DROP USER ''@'$(hostname)';
 	DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 	DROP DATABASE test;
 	FLUSH PRIVILEGES;
@@ -23,6 +24,7 @@ fi
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 
 	sqlcommands="
+	FLUSH PRIVILEGES;
 	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
