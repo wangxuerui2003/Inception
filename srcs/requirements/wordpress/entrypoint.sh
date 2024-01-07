c504eb049f2e90eb1dd282f82c365bf14cb9c11f
Wang Xue Rui <wangxuerui2003@gmail.com>
2024-01-07 12:57:42 +0800

    Update the installation method of wordpress and add redis plugin

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -3,27 +3,30 @@
 # If no wordpress file, download and configure the wp-config.php file
 if [ ! -e "./wp-config.php" ]; then    
 
-	# Get wordpress files
-	curl -k https://wordpress.org/latest.tar.gz | tar zx --strip-components=1 -C .
+	wp core download --allow-root
 
-	# Configure database connection credentials
-	sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" wp-config-sample.php
-	sed -i "s/username_here/$WORDPRESS_DB_USER/" wp-config-sample.php
-	sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" wp-config-sample.php
-	sed -i "s/localhost/$WORDPRESS_DB_HOST/" wp-config-sample.php
+	wp config create --dbname=$WORDPRESS_DB_NAME \
+		--dbuser=$WORDPRESS_DB_USER \
+		--dbpass=$WORDPRESS_DB_PASSWORD \
+		--dbhost=$WORDPRESS_DB_HOST \
+		--allow-root
 
-	# Generate the random salts for security in wordpress
-	awk '
-		/put your unique phrase here/ {
-			cmd = "head -c1m /dev/urandom | sha1sum | cut -d\\  -f1"
-			cmd | getline str
-			close(cmd)
-			gsub("put your unique phrase here", str)
-		}
-		{ print }
-	' wp-config-sample.php > wp-config.php
+	wp config set WP_REDIS_HOST redis --allow-root
+	wp config set WP_REDIS_PORT 6379 --allow-root
 
-	rm wp-config-sample.php
+	wp core install --url=wxuerui.42.fr \
+		--title="Inception" \
+		--admin_user=$WP_ADMIN_USER \
+		--admin_password=$WP_ADMIN_PASSWORD \
+		--admin_email=$WP_ADMIN_EMAIL \
+		--allow-root
+
+	wp plugin install redis-cache --allow-root
+	wp plugin update --all --allow-root
+	wp plugin activate redis-cache --allow-root
+	wp redis enable --allow-root
+	wp cache flush --allow-root
+
 
 	chown -R www-data:www-data .
 fi
